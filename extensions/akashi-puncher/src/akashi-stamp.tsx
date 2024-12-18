55f259834db68c435487967a39f339ca62f1cb8a
taconasu <smi.git.se@gmail.com>
2024-12-18 11:09:55 +0900

    打刻種別を選んでsubmitできるI/Fを用意

---
+++ extensions/akashi-puncher/src/akashi-stamp.tsx
@@ -0,0 +1,47 @@
+import { Action, ActionPanel, Form, getPreferenceValues } from "@raycast/api";
+import { FormValidation, useForm } from "@raycast/utils";
+import { STAMP_TYPE, StampType } from "./constants";
+import { stamp } from "./repositories/akashi/stampRepository";
+import { STAMP_TYPE_LABELS } from "./constants/StampType";
+
+type StampFormValues = {
+  type: string;
+};
+
+const Main = () => {
+  const { Domain, CompanyId, APIToken } = getPreferenceValues<Preferences.AkashiStamp>();
+
+  const { handleSubmit, itemProps } = useForm<StampFormValues>({
+    onSubmit: async (values) => {
+      // FIXME: Form.Dropdownがstring型しか許容しないため、暫定的に無理やりキャストしている
+      await stamp(Domain, CompanyId, APIToken, values.type as unknown as StampType, "時刻");
+    },
+    validation: {
+      type: FormValidation.Required,
+    },
+  });
+
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="打刻" onSubmit={handleSubmit} />
+        </ActionPanel>
+      }
+    >
+      <Form.Dropdown title="打刻種別" {...itemProps.type}>
+        {Object.values(STAMP_TYPE).map((stampType) => {
+          return (
+            <Form.Dropdown.Item
+              value={String(stampType)}
+              title={STAMP_TYPE_LABELS.find((label) => label.value === stampType)?.label || ""}
+              key={stampType}
+            />
+          );
+        })}
+      </Form.Dropdown>
+    </Form>
+  );
+};
+
+export default Main;
--- extensions/akashi-puncher/src/constants/StampType.ts
+++ extensions/akashi-puncher/src/constants/StampType.ts
@@ -6,5 +6,13 @@ export const STAMP_TYPE = {
   REST_IN: 31,
   REST_OUT: 32,
 } as const;
+export const STAMP_TYPE_LABELS = [
+  { value: STAMP_TYPE.PUNCH_IN, label: "出勤" },
+  { value: STAMP_TYPE.PUNCH_OUT, label: "退勤" },
+  { value: STAMP_TYPE.GO_STRAIGHT, label: "直行" },
+  { value: STAMP_TYPE.GO_STRAIGHT_HOME, label: "直帰" },
+  { value: STAMP_TYPE.REST_IN, label: "休憩入り" },
+  { value: STAMP_TYPE.REST_OUT, label: "休憩戻り" },
+];
 
 export type StampType = (typeof STAMP_TYPE)[keyof typeof STAMP_TYPE];
