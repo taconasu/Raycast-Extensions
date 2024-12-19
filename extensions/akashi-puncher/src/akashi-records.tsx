4ccfaca8180f78cc8d71ebe8879fb6f717f4052f
taconasu <smi.git.se@gmail.com>
2024-12-19 18:45:50 +0900

    add Open in Browser for akashi-records

--- extensions/akashi-puncher/src/akashi-records.tsx
+++ extensions/akashi-puncher/src/akashi-records.tsx
@@ -1,9 +1,16 @@
 import { useFetch, usePromise } from "@raycast/utils";
 import { fetchMe, RecordsResponse } from "./repositories/akashi/stampRepository";
-import { getPreferenceValues, List } from "@raycast/api";
+import { Action, ActionPanel, getPreferenceValues, List } from "@raycast/api";
 import { useMemo } from "react";
 import { dayjs } from "./lib/dayjs";
 import { PATH } from "./constants";
+
+const RecordActionPanel = () => (
+  <ActionPanel title="">
+    {/* eslint-disable-next-line @raycast/prefer-title-case */}
+    <Action.OpenInBrowser title="Open AKASHI in Browser" url="https://atnd.ak4.jp/ja/attendance" />
+  </ActionPanel>
+);
 
 const Records = () => {
   const { Domain, CompanyId, APIToken } = getPreferenceValues<Preferences.AkashiRecords>();
@@ -32,15 +39,26 @@ const Records = () => {
         <>
           {workingRecords.toReversed().map((record) => (
             <List.Section key={record.date} title={record.date}>
-              {record.start_time && <List.Item title={`🏃‍♂️ 出勤: ${dayjs(record.start_time).format("HH:mm")}`} />}
+              {record.start_time && (
+                <List.Item
+                  title={`🏃‍♂️ 出勤: ${dayjs(record.start_time).format("HH:mm")}`}
+                  actions={<RecordActionPanel />}
+                />
+              )}
               {record.break_time_results.length > 0 &&
                 record.break_time_results.map((breakTime, breakIndex) => (
                   <List.Item
                     key={breakIndex}
                     title={`☕️ 休憩: ${dayjs(breakTime.result_break_time_start_time).format("HH:mm")} - ${dayjs(breakTime.result_break_time_end_time).format("HH:mm")}`}
+                    actions={<RecordActionPanel />}
                   />
                 ))}
-              {record.end_time && <List.Item title={`🚗 退勤: ${dayjs(record.end_time).format("HH:mm")}`} />}
+              {record.end_time && (
+                <List.Item
+                  title={`🚗 退勤: ${dayjs(record.end_time).format("HH:mm")}`}
+                  actions={<RecordActionPanel />}
+                />
+              )}
             </List.Section>
           ))}
         </>
